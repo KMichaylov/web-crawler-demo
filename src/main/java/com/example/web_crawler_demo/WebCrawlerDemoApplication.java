857795e45121d44174f2b50937409f76cb4e3a9d
Kristiyan Michaylov <krisidm24@gmail.com>
2025-05-11 23:26:57 +0200

    Initial boilerplate commit

---
+++ src/main/java/com/example/web_crawler_demo/WebCrawlerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_crawler_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCrawlerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCrawlerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/web_crawler_demo/WebCrawlerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_crawler_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrawlerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
