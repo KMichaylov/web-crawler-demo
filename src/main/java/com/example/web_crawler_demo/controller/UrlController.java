ec1fc174da7974b7a4ef917aaf4cd7b086bd8e11
Kristiyan Michaylov <krisidm24@gmail.com>
2025-05-13 23:10:28 +0200

    Add version number to the rest controller

--- src/main/java/com/example/web_crawler_demo/controller/UrlController.java
+++ src/main/java/com/example/web_crawler_demo/controller/UrlController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/url")
+@RequestMapping("/api/v1/url")
 public class UrlController {
 
     private final UrlService urlService;
