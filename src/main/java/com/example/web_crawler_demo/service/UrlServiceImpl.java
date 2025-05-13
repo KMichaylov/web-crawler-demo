e4777d9cf3df1ca3bda56ff03a6131fc7d618d74
Kristiyan Michaylov <krisidm24@gmail.com>
2025-05-13 22:59:48 +0200

    Add initial working functionality to crawler using JSoup

--- src/main/java/com/example/web_crawler_demo/service/UrlServiceImpl.java
+++ src/main/java/com/example/web_crawler_demo/service/UrlServiceImpl.java
@@ -1,15 +1,20 @@
 package com.example.web_crawler_demo.service;
 
 import com.example.web_crawler_demo.response.CrawlerResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.*;
 
 @Service
 public class UrlServiceImpl implements UrlService {
-    //    We need to crawl only pages from the same domain!!!
-    private Set<String> visitedPages;
+    private final Set<String> visitedPages;
     private String domain;
 
 
@@ -20,12 +25,39 @@ public class UrlServiceImpl implements UrlService {
 
     @Override
     public Optional<CrawlerResponse> crawl(String url) {
-
-        return Optional.of(new CrawlerResponse("We are fine", List.of("blabal", "blabla")));
+        try {
+            URI uri = new URI(url);
+            domain = uri.getScheme() + "://" + uri.getHost();
+            visitedPages.clear();
+            List<String> pages = new ArrayList<>();
+            crawlPage(url, pages);
+            return Optional.of(new CrawlerResponse(domain, pages));
+        } catch (URISyntaxException e) {
+            System.out.println(e.getMessage());
+            return Optional.empty();
+        }
     }
 
-    private void crawPage(String domainUrl, ArrayList<String> urls) {
+    private void crawlPage(String baseUrl, List<String> urls) {
+        if (visitedPages.contains(baseUrl) || !baseUrl.startsWith(domain)) {
+            return;
+        }
+        try {
+            // Get the whole html page for the link
+            Document doc = Jsoup.connect(baseUrl).get();
+            visitedPages.add(baseUrl);
+            urls.add(baseUrl);
+            System.out.println(doc.body());
 
+            // Go through each link on the page and nest inside it
+            Elements linksOnPage = doc.select("a[href]");
+            for (Element link : linksOnPage) {
+                String absoluteUrl = link.absUrl("href");
+                crawlPage(absoluteUrl, urls);
+            }
+        } catch (IOException e) {
+            System.err.println("Something went wrong with processing" + baseUrl + ": " + e.getMessage());
+        }
     }
 
 }
