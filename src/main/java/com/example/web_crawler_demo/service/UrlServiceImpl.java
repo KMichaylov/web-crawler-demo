1632788d1be2a7b757c94dc4c66fcd96726157af
Kristiyan Michaylov <krisidm24@gmail.com>
2025-05-13 22:35:03 +0200

    Add required functions for UrlServiceImpl

--- src/main/java/com/example/web_crawler_demo/service/UrlServiceImpl.java
+++ src/main/java/com/example/web_crawler_demo/service/UrlServiceImpl.java
@@ -1,17 +1,31 @@
 package com.example.web_crawler_demo.service;
 
 import com.example.web_crawler_demo.response.CrawlerResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class UrlServiceImpl implements UrlService {
+    //    We need to crawl only pages from the same domain!!!
+    private Set<String> visitedPages;
+    private String domain;
+
+
+    public UrlServiceImpl() {
+        this.visitedPages = new HashSet<>();
+        this.domain = "";
+    }
+
     @Override
     public Optional<CrawlerResponse> crawl(String url) {
+
         return Optional.of(new CrawlerResponse("We are fine", List.of("blabal", "blabla")));
+    }
+
+    private void crawPage(String domainUrl, ArrayList<String> urls) {
+
     }
 
 }
